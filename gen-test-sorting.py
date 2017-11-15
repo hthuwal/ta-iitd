8bceea7e016220c9dee55ee057481c81bb18ca93
Harish <hthuwal@gmail.com>
2017-11-15 13:23:40 +0530

    Gen Test Cases Sorting

--- gen-test-sorting.py
+++ gen-test-sorting.py
@@ -22,9 +22,10 @@ output = %s
 
 def dob(record):
     record = record.split('\t')
-    # date = record[1]
-    # d, m, y = date[0:2], date[2:4], date[4:]
-    return record[0].strip()
+    date = record[1]
+    d, m, y = date[0:2], date[2:4], date[4:]
+    return y+m+d
+    # return record[0].strip()
 
 left = 0
 right = len(stud_records)
@@ -38,13 +39,13 @@ for i in range(1, 1 + NUM_CASES):
 
     ans = sorted(stud_records[left:right], key=dob)
     out += '\n'.join(ans)
-
+    inp = inp.replace('\t', ' ')
+    out = out.replace('\t', ' ')
     print(TEST_CASE_FORMAT % (i, inp, out))
 
     left = randint(0, len(stud_records))
     right = randint(left, len(stud_records))
     len_input = right - left
-    
+
     # assert(len_input, len(stud_records[left:right]))
     # print(inp)
-
