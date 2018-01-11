73e12b48e918e391a887c7799bb0d65aa89456e5
Harish <hthuwal@gmail.com>
2018-01-11 15:59:51 +0530

    model solutions for mcuberoot and nlog

--- lab2/model_solutions.ml
+++ lab2/model_solutions.ml
@@ -8,3 +8,10 @@ let model_degrees_to_radians angle:float =
 	let pi = 4.0 *. (atan 1.0) in
 		((pi) /. (180.0)) *. angle;;
 
+(* model solution for natural logarithm *)
+let model_nlog x:float = 
+	log (x +. sqrt(x *. x +. 1.0));;
+
+(* model solution for cube root *)
+let model_mcuberoot (x:float) (y:float) (z:float) = 
+	(x*.y*.z)**(1.0/.3.0);;
