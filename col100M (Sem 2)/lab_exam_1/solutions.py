50362a393daf44442120066635e863949bdfe6d8
Harish <hthuwal@gmail.com>
2018-01-23 23:13:08 +0530

    min_count takes function as parameter

--- col100M (Sem 2)/lab_exam_1/solutions.py
+++ col100M (Sem 2)/lab_exam_1/solutions.py
@@ -12,14 +12,15 @@ def num_ways(amount, coins, i):
     return num_ways(amount, coins, i+1) + num_ways(amount - coins[i], coins, i)
 
 
-def min_count(amount, coins, i):
+def min_count(amount, coins, i, func):
     if amount == 0:
         return 0
 
     if amount < 0 or i >= len(coins):
         return sys.maxsize
 
-    ans1 = min_count(amount - coins[i], coins, i)
+    ans1 = min_count(amount - coins[i], coins, i, func)
     if ans1 != sys.maxsize:
-        ans1 += 1
-    return min(ans1, min_count(amount, coins, i+1))
+        ans1 += func(coins[i])
+    return min(ans1, min_count(amount, coins, i+1, func))
+
