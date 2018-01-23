94529837a13e641ddf731c506a6f2e32db165584
Harish <hthuwal@gmail.com>
2018-01-24 01:50:23 +0530

    Add a main function that reads test cases from file

--- col100M (Sem 2)/lab_exam_1/solutions.py
+++ col100M (Sem 2)/lab_exam_1/solutions.py
@@ -1,4 +1,4 @@
-import sys
+OCAML_MAX = 4611686018427387903
 
 
 def num_ways(amount, coins, i):
@@ -17,10 +17,23 @@ def min_count(amount, coins, i, func):
         return 0
 
     if amount < 0 or i >= len(coins):
-        return sys.maxsize
+        return OCAML_MAX
 
     ans1 = min_count(amount - coins[i], coins, i, func)
-    if ans1 != sys.maxsize:
+    if ans1 != OCAML_MAX:
         ans1 += func(coins[i])
     return min(ans1, min_count(amount, coins, i+1, func))
 
+
+if __name__ == '__main__':
+    f = open("in.txt")
+    lines = f.readlines()
+    for line in lines:
+        line = map(int, line.split())
+        amount = line[0]
+        coins = line[1:5]
+        func = line[5]
+        if func == 0:
+            print num_ways(amount, coins, 0), min_count(amount, coins, 0, (lambda x: 2*x))
+        else:
+            print num_ways(amount, coins, 0), min_count(amount, coins, 0, (lambda x: 100-x))
