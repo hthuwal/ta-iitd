0d930b5f41e242112f7023224351acdb5f2fe84b
Harish <hthuwal@gmail.com>
2018-01-24 01:49:24 +0530

    Minor Changes - coins should be in range lower than amount

--- col100M (Sem 2)/lab_exam_1/testcasegenerator.py
+++ col100M (Sem 2)/lab_exam_1/testcasegenerator.py
@@ -1,13 +1,14 @@
 from random import randint
 
 NUM_CASES = 50
-NUM_RANGE = (1, 15)
+NUM_RANGE = (1, 50)
+NUM_RANGE2 = (1, 15)
 
 for i in range(0, NUM_CASES):
     amt = randint(*NUM_RANGE)
-    a = randint(*NUM_RANGE)
-    b = randint(*NUM_RANGE)
-    c = randint(*NUM_RANGE)
-    d = randint(*NUM_RANGE)
+    a = randint(*NUM_RANGE2)
+    b = randint(*NUM_RANGE2)
+    c = randint(*NUM_RANGE2)
+    d = randint(*NUM_RANGE2)
     f = randint(0, 1)
     print amt, a, b, c, d, f
