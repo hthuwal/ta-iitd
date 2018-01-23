35f7e608974ffb82becf4e57f02bcc245eb3f44d
Harish <hthuwal@gmail.com>
2018-01-23 22:43:57 +0530

    Model solution for min_count

--- col100M (Sem 2)/lab_exam_1/solutions.py
+++ col100M (Sem 2)/lab_exam_1/solutions.py
@@ -1,3 +1,6 @@
+import sys
+
+
 def num_ways(amount, coins, i):
 
     if amount == 0:
@@ -7,3 +10,16 @@ def num_ways(amount, coins, i):
         return 0
 
     return num_ways(amount, coins, i+1) + num_ways(amount - coins[i], coins, i)
+
+
+def min_count(amount, coins, i):
+    if amount == 0:
+        return 0
+
+    if amount < 0 or i >= len(coins):
+        return sys.maxsize
+
+    ans1 = min_count(amount - coins[i], coins, i)
+    if ans1 != sys.maxsize:
+        ans1 += 1
+    return min(ans1, min_count(amount, coins, i+1))
