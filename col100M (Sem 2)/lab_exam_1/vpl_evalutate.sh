649c066bcada58cc6dbcee9aebbb74091b0d9cc1
Harish <hthuwal@gmail.com>
2018-02-05 23:21:47 +0530

    Evaluate only test1.ml, Read test cases from file and call checkers one by one
    Each test case evaluated seperately
    -Handle timeout
    -Stackoverflow

--- col100M (Sem 2)/lab_exam_1/vpl_evalutate.sh
+++ col100M (Sem 2)/lab_exam_1/vpl_evalutate.sh
@@ -1,69 +1,59 @@
-#!/bin/bash
-grade1=0.0
-grade2=0.0
-time_limit=5 #sec
-flag=0
+# #!/bin/bash
+testno=0
+time_limit=1.5 #sec
+testcase_file="in.txt"
+result_file="result.txt"
+grade=0.0
 
+if [ -f $result_file ]
+then
+    rm $result_file
+fi
 # Evaluation of test1.ml
-ocamlc -o out1 test1.ml run_test1.ml
+
+compilation=$(ocamlc -o out1 test1.ml run_test1.ml 2>&1) # if compilation is successful out1 file is created
+
 if [ -f "out1" ]
 then
+    printf "Compilation Successful! Evaluating Code\n"
 
-    grade0=`timeout $time_limit ./out1 "invalid.cases"`
-    exit_status=$?
-    if [ $exit_status -eq 124 ]
-    then
-        grade0=0.0
-        flag=$((flag+1))
-        echo "Execution Timeout (>$time_limit sec) while evaluating test1.ml"
-        
-    fi
-    
-    grade1=`timeout $time_limit ./out1 "vpl_evaluate.cases"`
-    exit_status=$?
-    if [ $exit_status -eq 124 ]
-    then
-        grade1=0.0
-        flag=$((flag+1))
-        echo "Execution Timeout (>$time_limit sec) while evaluating test1.ml"
-    fi
-    
-    grade1=$(echo $grade1 + $grade0 | bc)
-    if [ $flag -lt 1 ]
-    then
-        if [ 1 -eq "$(echo "${grade1} < 10.0" | bc)" ]
+    while IFS= read line
+    do
+
+        testno=$((testno+1))
+        arr=($line) #splitting line into elements
+        printf "Test case $testno: coinChanger ${arr[*]}\n"
+
+        execution=$(timeout $time_limit ./out1 ${arr[*]} 2>&1)
+        exit_status=$?
+
+        if [ $exit_status -eq 124 ] #timeout occured
         then
-            echo "Wrong output for some test cases of test1.ml"
+            printf "Execution Timeout (>$time_limit sec) while evaluating test1.ml\n\n"
+        
+        elif [ $exit_status -eq 0 ] #No runtime error occured (Correct or wrong answer)
+        then
+            printf "$execution\n\n"
+        
+        else #some error occured for this test case
+            printf "Runtime Error: $execution\n\n"
         fi
-    fi
+
+    done <"$testcase_file"
+
+else
+    printf "Compilation failed with the following error:\n$compilation\n"
 fi
 
-# evaluation of test2.ml
-flag=0
-ocamlc -o out2 test2.ml run_test2.ml
-if [ -f "out2" ]
-then 
-    grade2=`timeout $time_limit ./out2`
-    exit_status=$?
-    if [ $exit_status -eq 124 ]
-    then
-        grade2=0.0
-        flag=$((flag+1))
-        echo "Execution Timeout (>$time_limit sec) while evaluating test2.ml"
-        
-    fi
-    
-    if [ $flag -lt 1 ]
-    then
-        if [ 1 -eq "$(echo "${grade2} < 5.0" | bc)" ]
-        then
-            echo "Wrong output for some teset cases of test2.ml"
-        fi
-    fi
-fi
 
-grade=$(echo $grade1 + $grade2 | bc)
 
-echo "#!/bin/bash" > vpl_execution
+while IFS= read line
+do
+    # echo $line
+    grade=$(echo $grade + $line | bc)
+done <"$result_file"
+
+echo $grade
+echo "#! /bin/bash" > vpl_execution
 echo 'echo "Grade :=>> ' $grade\" >> vpl_execution
 chmod +x vpl_execution
