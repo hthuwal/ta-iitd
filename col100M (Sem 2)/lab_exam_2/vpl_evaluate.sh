054bbd88a621032b8d54f070004acabe0a9537f7
Harish Thuwal <hthuwal@gmail.com>
2018-03-16 00:57:15 +0530

    Improvements

--- col100M (Sem 2)/lab_exam_2/vpl_evaluate.sh
+++ col100M (Sem 2)/lab_exam_2/vpl_evaluate.sh
@@ -11,6 +11,15 @@ tf4="cases4.txt"
 tf5a="cases5a.txt"
 tf5b="cases5b.txt"
 
+g1=0.0
+g2a=0.0
+g2b=0.0
+g2c=0.0
+g3=0.0
+g4=0.0
+g5a=0.0
+g5b=0.0
+
 if [ -f $result_file ]
 then
     rm $result_file
@@ -34,7 +43,7 @@ function assess(){
 }
 
 function do_grading(){
-    temp_grade=0.0
+    local temp_grade=0.0
     if [ -f "$result_file" ]
     then
         while IFS= read line
@@ -43,6 +52,7 @@ function do_grading(){
         done <"$result_file"
     fi
     rm "$result_file"
+    echo "$temp_grade"
 }
 
 
@@ -50,6 +60,7 @@ printf "\ntest1.ml\n"
 compilation=$(ocamlc -o out test1.ml model.ml run_test1.ml 2>&1) # if compilation is successful out file is created
 
 testno=0
+num_corr=0
 if [ -f "out" ]
 then
     printf "Compilation Successful! Evaluating Code\n"
@@ -58,186 +69,261 @@ then
     do
         testno=$((testno+1))
         arr=($line) #splitting line into elements
-        printf "Test case $testno\n"
         execution=$(timeout $time_limit ./out ${arr[*]} 2>&1)
         exit_status=$?
         assess "$exit_status" "$execution"
     done <"$tf1"
-else
-    printf "Compilation failed with the following error:\n$compilation\n"
-fi
-do_grading
-g1=$temp_grade
-num_corr=$(echo $g1 / 0.1 | bc)
-printf "$num_corr/20 Correct\n"
 
-rm out
+    if [ -f $result_file ]
+    then
+        g1=$(do_grading)
+    else
+        g1=0
+    fi
+    num_corr=$(echo $g1 / 0.1 | bc)
+    printf "$num_corr/20 Correct\n"
+
+    if [ -f out ]
+    then
+        rm out
+    fi
+else
+    printf "Compilation failed with the following error:\n$compilation\n\n"
+fi
+
+
 printf "\ntest2.ml\n"
+printf "\nTesting swap\n"
 compilation=$(ocamlc -o out test2.ml model.ml run_test2_swap.ml 2>&1) # if compilation is successful out file is created
 testno=0
+num_corr=0
 if [ -f "out" ]
 then
     printf "Compilation Successful! Evaluating Code\n"
-    printf "\nTesting swap\n"
 
     while IFS= read line
     do
         testno=$((testno+1))
         arr=($line) #splitting line into elements
-        printf "Test case $testno\n"
         execution=$(timeout $time_limit ./out ${arr[*]} 2>&1)
         exit_status=$?
         assess "$exit_status" "$execution"
     done <"$tf2a"
-else
-    printf "Compilation failed with the following error:\n$compilation\n"
-fi
-do_grading
-g2a=$temp_grade
-gpt=0.1
-num_corr=$(echo $g2a / $gpt | bc)
-printf "$num_corr/60 Correct\n"
 
+    if [ -f $result_file ]
+    then
+        g2a=$(do_grading)
+    else
+        g2a=0
+    fi
+    gpt=0.1
+    num_corr=$(echo $g2a / $gpt | bc)
+    printf "$num_corr/60 Correct\n"
+
+    if [ -f out ]
+    then
+        rm out
+    fi
+else
+    printf "Compilation failed with the following error:\n$compilation\n\n"
+fi
+
+
+printf "\nTesting mult\n"
 compilation=$(ocamlc -o out test2.ml model.ml run_test2_mult.ml 2>&1) # if compilation is successful out file is created
 testno=0
+num_corr=0
 if [ -f "out" ]
 then
-    printf "\nTesting mult\n"
 
     while IFS= read line
     do
         testno=$((testno+1))
         arr=($line) #splitting line into elements
-        printf "Test case $testno\n"
         execution=$(timeout $time_limit ./out ${arr[*]} 2>&1)
         exit_status=$?
         assess "$exit_status" "$execution"
     done <"$tf2b"
 
-else
-    printf "Compilation failed with the following error:\n$compilation\n"
-fi
-do_grading
-g2b=$temp_grade
-num_corr=$(echo $g2b / 0.1 | bc)
-printf "$num_corr/60 Correct\n"
+    if [ -f $result_file ]
+    then
+        g2b=$(do_grading)
+    else
+        g2b=0
+    fi
+    num_corr=$(echo $g2b / 0.1 | bc)
+    printf "$num_corr/60 Correct\n"
 
+    if [ -f out ]
+    then
+        rm out
+    fi
+else
+    printf "Compilation failed with the following error:\n$compilation\n\n"
+fi
+
+
+printf "\nTesting addRows\n"
 compilation=$(ocamlc -o out test2.ml model.ml run_test2_addrows.ml 2>&1) # if compilation is successful out file is created
 testno=0
+num_corr=0
 if [ -f "out" ]
 then
-    printf "\nTesting addRows\n"
 
     while IFS= read line
     do
         testno=$((testno+1))
         arr=($line) #splitting line into elements
-        printf "Test case $testno\n"
         execution=$(timeout $time_limit ./out ${arr[*]} 2>&1)
         exit_status=$?
         assess "$exit_status" "$execution"
     done <"$tf2c"
 
+    if [ -f $result_file ]
+    then
+        g2c=$(do_grading)
+    else
+        g2c=0
+    fi
+    num_corr=$(echo $g2c / 0.1 | bc)
+    printf "$num_corr/60 Correct\n"
+
+    if [ -f out ]
+    then
+        rm out
+    fi
 else
-    printf "Compilation failed with the following error:\n$compilation\n"
+    printf "Compilation failed with the following error:\n$compilation\n\n"
 fi
-do_grading
-g2c=$temp_grade
-num_corr=$(echo $g2c / 0.1 | bc)
-printf "$num_corr/60 Correct\n"
+
 
 printf "\ntest3.ml\n"
 compilation=$(ocamlc -o out test3.ml model.ml run_test3.ml 2>&1) # if compilation is successful out file is created
 testno=0
+num_corr=0
 if [ -f "out" ]
 then
     while IFS= read line
     do
         testno=$((testno+1))
         arr=($line) #splitting line into elements
-        printf "Test case $testno\n"
         execution=$(timeout $time_limit ./out ${arr[*]} 2>&1)
         exit_status=$?
         assess "$exit_status" "$execution"
     done <"$tf3"
 
+    if [ -f $result_file ]
+    then
+        g3=$(do_grading)
+    else
+        g3=0
+    fi
+    num_corr=$(echo $g3 / 0.1 | bc)
+    printf "$num_corr/100 Correct\n"
+
+    if [ -f out ]
+    then
+        rm out  
+    fi
+
 else
-    printf "Compilation failed with the following error:\n$compilation\n"
+    printf "Compilation failed with the following error:\n$compilation\n\n"
 fi
-do_grading
-g3=$temp_grade
-num_corr=$(echo $g3 / 0.1 | bc)
-printf "$num_corr/100 Correct\n"
 
 printf "\ntest4.ml\n"
 compilation=$(ocamlc -o out test4.ml model.ml run_test4.ml 2>&1) # if compilation is successful out file is created
 testno=0
+num_corr=0
 if [ -f "out" ]
 then
     while IFS= read line
     do
         testno=$((testno+1))
         arr=($line) #splitting line into elements
-        printf "Test case $testno\n"
         execution=$(timeout $time_limit ./out ${arr[*]} 2>&1)
         exit_status=$?
         assess "$exit_status" "$execution"
     done <"$tf4"
 
+    if [ -f $result_file ]
+    then
+        g4=$(do_grading)
+    else
+        g4=0
+    fi
+    num_corr=$(echo $g4 / 0.1 | bc)
+    printf "$num_corr/50 Correct\n"
+
+    if [ -f out ]
+    then
+        rm out
+    fi
+
 else
-    printf "Compilation failed with the following error:\n$compilation\n"
+    printf "Compilation failed with the following error:\n$compilation\n\n"
 fi
-do_grading
-g4=$temp_grade
-num_corr=$(echo $g4 / 0.1 | bc)
-printf "$num_corr/50 Correct\n"
 
 printf "\ntest5.ml\n"
+printf "\nTesting solveRowEchelon\n"
 compilation=$(ocamlc -o out test5.ml model.ml run_test5a.ml 2>&1) # if compilation is successful out file is created
 testno=0
+num_corr=0
 if [ -f "out" ]
 then
-    printf "\nTesting solveRowEchelon\n"
 
     while IFS= read line
     do
         testno=$((testno+1))
         arr=($line) #splitting line into elements
-        printf "Test case $testno\n"
         execution=$(timeout $time_limit ./out ${arr[*]} 2>&1)
         exit_status=$?
         assess "$exit_status" "$execution"
     done <"$tf5a"
 
-else
-    printf "Compilation failed with the following error:\n$compilation\n"
-fi
-do_grading
-g5a=$temp_grade
-num_corr=$(echo $g5a / 0.1 | bc)
-printf "$num_corr/50 Correct\n"
+    if [ -f $result_file ]
+    then
+        g5a=$(do_grading)
+    else
+        g5a=0
+    fi
+    num_corr=$(echo $g5a / 0.1 | bc)
+    printf "$num_corr/50 Correct\n"
 
+    if [ -f out ]
+    then
+        rm out
+    fi
+else
+    printf "Compilation failed with the following error:\n$compilation\n\n"
+fi
+
+
+printf "\nTesting solve\n"
 compilation=$(ocamlc -o out test5.ml model.ml run_test5b.ml 2>&1) # if compilation is successful out file is created
 testno=0
+num_corr=0
 if [ -f "out" ]
 then
-    printf "\nTesting solve\n"
 
     while IFS= read line
     do
         testno=$((testno+1))
         arr=($line) #splitting line into elements
-        printf "Test case $testno\n"
         execution=$(timeout $time_limit ./out ${arr[*]} 2>&1)
         exit_status=$?
         assess "$exit_status" "$execution"
     done <"$tf5b"
 
+    if [ -f $result_file ]
+    then
+        g5b=$(do_grading)
+    else
+        g5b=0
+    fi
 else
-    printf "Compilation failed with the following error:\n$compilation\n"
+    printf "Compilation failed with the following error:\n$compilation\n\n"
 fi
-do_grading
-g5b=$temp_grade
+
 num_corr=$(echo $g5b / 0.1 | bc)
 printf "$num_corr/50 Correct\n"
 
