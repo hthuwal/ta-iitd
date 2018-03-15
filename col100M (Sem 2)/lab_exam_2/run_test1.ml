c542d6e36ed5893603b1dd607c20bb1e8359fcd4
Harish Thuwal <hthuwal@gmail.com>
2018-03-15 18:40:59 +0530

    I guess this is done

--- col100M (Sem 2)/lab_exam_2/run_test1.ml
+++ col100M (Sem 2)/lab_exam_2/run_test1.ml
@@ -1,21 +1,27 @@
-(* open Test1 *)
-open Model
 open Array
 open Printf
 
 let gpt = 0.1;;
 
-let test1_runner = 
-	let student = Test1.coinChanger amt a b c d in
-	let ta = ways amt a b c d in
-	if student == ta then (print_string "Correct Answer...\n\n"; gpt) 
-	else (print_error (string_of_int student) (string_of_int ta); 0.0);;;;
+let test1_runner mat b = 
+	let student = Test1.checkDimension mat b in
+	let ta = Model.checkDimension mat b in
+	if student = ta then gpt
+	else 0.0;;
 
-let rec print_list l = match l with
-[] -> ()
-|x::xs -> print_float(x);print_string " " ;print_list xs;;
+let rec remove_last l ans= match l with
+[] -> []
+|x::y::[] -> ans @ [x]
+|x::tl -> remove_last tl (ans @ [x]);;
 
-let rec print_mat m = match m with
-[] -> ()
-|row::xs -> print_list row; print_newline();print_mat xs;;
+let mat = Model.read_mat_from_carg;;
 
+let b = List.nth mat ((List.length mat) - 1);;
+let mat = remove_last mat [];;
+
+let file = "result.txt";;
+let () =
+  (* Write message to file *)
+  let oc = open_out_gen [Open_creat; Open_text; Open_append] 0o640 file in  (* create or truncate file, return channel *)
+  fprintf oc "%f\n" (test1_runner mat b);   (* write something *)   
+  close_out oc;;
