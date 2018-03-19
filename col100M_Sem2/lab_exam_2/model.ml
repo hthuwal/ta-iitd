b690fad8e034780c71781881766b55e4f7e43ec8
Harish Thuwal <hthuwal@gmail.com>
2018-03-19 18:14:42 +0530

    Final Model File with 10^-10 restrictions on precision and some correction

--- col100M_Sem2/lab_exam_2/model.ml
+++ col100M_Sem2/lab_exam_2/model.ml
@@ -22,6 +22,12 @@ let read_mat_from_carg =
 			read_mat_helper [] flat 0;;
 
 
+let epsilon = 10. ** (-10.)
+
+let float_zero x = abs_float(x) <= epsilon
+
+let check_zero x = if (float_zero x) then 0.0 else x;;
+
 let rec dimension l = 
 	match l with
 	| [] -> 0
@@ -30,7 +36,7 @@ let rec dimension l =
 let rec dimension2H mat dim = 
 	match mat with
 	| [] -> dim
-	| row1::rest -> if (dimension row1) = (snd dim)
+	| row1::rest -> if (dimension row1) == (snd dim)
 			then dimension2H rest ((1+(fst dim)),(snd dim))
 			else raise Not_a_matrix
 
@@ -43,13 +49,13 @@ let checkDimension mat b =
 	try
 		let m = (dimension2 mat) in
 			let n = (dimension b) in
-				if (fst m) = n then true
+				if (fst m) == n then true
 				else false
 	with e ->
 		false
 
 let rec insertH l i a = 
-	if i = 0 then
+	if i == 0 then
 		((a::(tl l)), (hd l))
 	else
 		let z = (insertH (tl l) (i-1) a) in
@@ -57,7 +63,7 @@ let rec insertH l i a =
 		
 		
 let rec swapH l i j =
-	if i = 0 then
+	if i == 0 then
 		let z = insertH (tl l) (j-1) (hd l) in
 			(snd z)::(fst z)
 	else
@@ -65,7 +71,7 @@ let rec swapH l i j =
 
 
 let swap l i j = 
-	if (i = j) then l
+	if (i == j) then l
 	else if (i < j) then swapH l i j
 	else swapH l j i	
 
@@ -76,7 +82,7 @@ let rec multH l c =
 	| hd::tl -> (hd *. c)::(multH tl c)
 
 let rec mult mat i c = 
-	if i = 0 then (multH (hd mat) c)::(tl mat)
+	if i == 0 then (multH (hd mat) c)::(tl mat)
 	else (hd mat)::(mult (tl mat) (i-1) c)
 
 let rec addLists l1 l2 = 
@@ -85,7 +91,7 @@ let rec addLists l1 l2 =
 	| h::t -> (h +. (hd l2))::(addLists t (tl l2))
 
 let rec replace l i x = 
-	if i = 0 then
+	if i == 0 then
 		x::(tl l)
 	else
 		(hd l):: (replace (tl l) (i-1) x)
@@ -98,7 +104,7 @@ let rec addRows mat i j =
 let rec firstNonZeroIndex l i = 
 	match l with
 	| [] -> max_int
-	| hd::tl -> if ( hd > 0. || hd < 0.) then i
+	| hd::tl -> if (not (float_zero hd)) then i
 		    else (firstNonZeroIndex tl (i+1))
 
 let rec firstNonZeroColumn mat =
@@ -108,8 +114,9 @@ let rec firstNonZeroColumn mat =
 	let k = (firstNonZeroColumn tl) in
 		if z < k then z else k
 
+
 let rec hasNonZeroIndex l i = 
-	if (i = 0) then let k = (hd l) in (k < 0. || k > 0.)
+	if (i == 0) then let k = (hd l) in (not (float_zero k))
 	else hasNonZeroIndex (tl l) (i-1)
 
 let rec firstRowWithLead mat i rowNum= 
@@ -120,7 +127,7 @@ let rec firstRowWithLead mat i rowNum=
 
 
 let rec normalizeH mat c j =
-	if (j = 0) then mat
+	if (j == 0) then mat
 	else
 		let mat0c = (nth (nth mat 0) c) in
 		let matjc = (nth (nth mat j) c) in
@@ -139,7 +146,7 @@ let rec rowEchelon mat =
 	| [] -> []
 	| _ ->
 	let c = firstNonZeroColumn mat in
-		if c = max_int then mat
+		if c == max_int then mat
 		else
 			let r = firstRowWithLead mat c 0 in
 			let mat1 = swap mat 0 r in
@@ -147,17 +154,17 @@ let rec rowEchelon mat =
 			(hd mat2)::(rowEchelon (tl mat2))
 
 
-let rec numSolutionsH mat c =
-	match mat with
-	| [] -> 1
-	| hd::tl -> let z = (firstNonZeroIndex hd 0) in
-		    if (c = z)
-		    then (numSolutionsH tl (c+1))
-		    else if ((length hd) = (z + 1)) then 0
-		    else max_int
+let rec numSolutionsH mat c maxCol =
+        match mat with
+        | [] -> if ((c+1) == maxCol) then 1 else max_int
+        | hd::tl -> let z = (firstNonZeroIndex hd 0) in
+                    if (c == z)
+                    then (numSolutionsH tl (c+1) maxCol)
+                    else if ((length hd) == (z + 1)) then 0
+                    else max_int
 
 
-let numSolutions mat = numSolutionsH (rowEchelon mat) 0
+let numSolutions mat = numSolutionsH (rowEchelon mat) 0 (dimension (hd mat))
 
 let rec mySum l = 
 	match l with
@@ -171,7 +178,7 @@ let rec solveEqnH a row currSol currSum =
 
 let rec solveEqn row currSol = 
 	let z = hd row in
-	if (z < 0. || z > 0.) then solveEqnH (hd row) (tl row) currSol 0.
+	if (not (float_zero z)) then solveEqnH (hd row) (tl row) currSol 0.
 	else solveEqn (tl row) currSol
 		
 
@@ -191,18 +198,23 @@ let rec createMat mat b =
 
 
 let solve mat b = 
-	if (checkDimension mat b) = false then raise Dimension_mismatch
+	if (checkDimension mat b) == false then raise Dimension_mismatch
 	else 
 		let mat1 = createMat mat b in
 		let n = numSolutions mat1 in
-		if n = 0 then raise No_solutions
-		else if n = max_int then raise Infinite_solutions
+		if n == 0 then raise No_solutions
+		else if n == max_int then raise Infinite_solutions
 		else solveRowEchelon (rowEchelon mat1)		
+		
 
+external format_float : string -> float -> string = "caml_format_float";;
+let string_of_float_2 f = valid_float_lexem (format_float "%.10g" f);;
+
+let apply l = List.map string_of_float_2 (List.map check_zero l);;
 
 let rec print_list l = match l with
 [] -> ()
-|x::xs -> print_float(x);print_string " " ;print_list xs;;
+|x::xs -> print_string(x);print_string " " ;print_list xs;;
 
 let rec print_mat m = match m with
 [] -> ()
