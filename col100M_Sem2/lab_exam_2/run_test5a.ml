2e09f5ff90261ba4305477ed6a36df7a5080e524
Harish Thuwal <hthuwal@gmail.com>
2018-03-19 18:07:38 +0530

    Add Print Logic

--- col100M_Sem2/lab_exam_2/run_test5a.ml
+++ col100M_Sem2/lab_exam_2/run_test5a.ml
@@ -1,13 +1,25 @@
 open Array
 open Printf
 
-let gpt = 0.1;;
+let gpt = 0.02;;
 
 let test5_runner mat= 
-	let student = Test5.solveRowEchelon mat in
-	let ta = Model.solveRowEchelon mat in
-	if student = ta then gpt
-	else 0.0;;
+	let student = Model.apply (Test5.solveRowEchelon mat) in
+	let ta = Model.apply (Model.solveRowEchelon mat) in
+	if student = ta then (print_string "CORRECT ANSWER\n"; gpt)
+	else
+	(
+		print_string "INCORRECT ANSWER\n"; 
+		print_string "MATRIX\n";
+		Model.print_mat (List.map Model.apply mat); 
+		print_string "\nEXPECTED ANSWER\n";
+		Model.print_list ta; 
+		print_string "\n\nYOUR ANSWER\n"; 
+		Model.print_list student; 
+		print_string "\n\n";0.0
+    );;
+
+
 
 let mat = Model.read_mat_from_carg;;
 
