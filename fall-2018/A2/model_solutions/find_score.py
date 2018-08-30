473968f72602769db6fd36a2caaa9a5cde254a0b
Harish Thuwal <hthuwal@gmail.com>
2018-08-30 15:08:33 +0530

    Script to find score from gold and pred label file

---
+++ fall-2018/A2/model_solutions/find_score.py
@@ -0,0 +1,14 @@
+import sys
+from sklearn.metrics import accuracy_score
+
+with open(sys.argv[1], "r") as f:
+    gold = f.readlines()
+    gold = [ int(each.strip()) for each in gold ]
+
+with open(sys.argv[1], "r") as f:
+    pred = f.readlines()
+    pred = [ int(each.strip()) for each in pred ]
+
+print(gold[:10])
+print(pred[:10])
+print(accuracy_score(gold, pred))
