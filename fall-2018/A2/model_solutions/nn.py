1d316cb40d11f981affd3a1ea21525915ad21e6e
Harish Thuwal <hthuwal@gmail.com>
2018-08-30 14:58:37 +0530

    Add function to train and predict the outcome

--- fall-2018/A2/model_solutions/nn.py
+++ fall-2018/A2/model_solutions/nn.py
@@ -56,6 +56,68 @@ class Neural_Network(nn.Module):
             out = layer(out)
         return out
 
+
+def predict(net, test_x):
+    test_x = torch.from_numpy(test_x).type(torch.FloatTensor)
+
+    pred = []
+    for i in (range(0, len(test_x), 100)):
+        x = test_x[i: i + 100]
+        x = Variable(x, volatile=False)
+        if use_cuda:
+            x = x.cuda()
+        outputs = net(x)
+        cur_pred = torch.max(outputs, dim=1)[1].data.cpu().numpy().tolist()
+        pred.extend(cur_pred)
+    return pred
+
+
+def train_and_predict(net, train_x, train_y, test_x, test_y, lr0, batch_size, num_epochs):
+    criterion = nn.MSELoss()
+    optimizer = torch.optim.SGD(net.parameters(), lr=lr0)
+
+    def hc_lambda(epoch):
+        return 1.0 / ((1 + epoch)**(0.5))
+        # return 1.0
+
+    scheduler = torch.optim.lr_scheduler.LambdaLR(optimizer, hc_lambda)
+
+    train_x = torch.from_numpy(train_x).type(torch.FloatTensor)
+    train_y = torch.from_numpy(train_y).type(torch.FloatTensor)
+
+    dataset = Data.TensorDataset(train_x, train_y)
+    train_loader = Data.DataLoader(dataset=dataset, batch_size=batch_size, shuffle=True)
+    for epoch in range(num_epochs):
+        net.train()
+        gold, pred, losses = [], [], []
+
+        for i, (x, y) in enumerate(train_loader):
+            print("\r%d" % (i), end="")
+            x, y = Variable(x), Variable(y)
+
+            if use_cuda:
+                x, y = x.cuda(), y.cuda()
+
+            outputs = net(x)
+            loss = criterion(outputs, y)
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+
+            cur_gold = torch.max(y, dim=1)[1].data.cpu().numpy().tolist()
+            cur_pred = torch.max(outputs, dim=1)[1].data.cpu().numpy().tolist()
+            gold.extend(cur_gold)
+            pred.extend(cur_pred)
+            losses.extend([loss.data.cpu().numpy()])
+
+        test_pred = predict(net, test_x)
+        print(" Epoch: %d Train Accuracy: %f Loss: %f Test Accuracy %f" % (epoch, accuracy_score(gold, pred), np.mean(losses), accuracy_score(test_y, test_pred)))
+        scheduler.step()
+
+    test_pred = predict(net, test_x)
+    return test_pred
+
+
 if __name__ == '__main__':
     train_x, train_y = read_data("col341_a2_data/devnagri_train.csv")
     lb = LabelBinarizer()
@@ -72,4 +134,9 @@ if __name__ == '__main__':
     # print(inp_size, output_size)
     hidden_layers = list(map(int, sys.argv[4:]))
     net = Neural_Network(activation, inp_size, hidden_layers, output_size)
-    print(net.layers)
+    if use_cuda:
+        net = net.cuda()
+    test_pred = train_and_predict(net, train_x, train_y, test_x, test_y, learning_rate, batch_size, 10)
+    with open("pred.txt", "w") as f:
+        for pred in test_pred:
+            f.write(str(pred) + "\n")
