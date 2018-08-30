d650f9e2c75f32a8eba0ba777fbcf9eab11cd6f7
Harish Thuwal <hthuwal@gmail.com>
2018-08-30 15:59:28 +0530

    Bug Fix and pep8

--- fall-2018/A2/model_solutions/find_score.py
+++ fall-2018/A2/model_solutions/find_score.py
@@ -3,11 +3,11 @@ from sklearn.metrics import accuracy_score
 
 with open(sys.argv[1], "r") as f:
     gold = f.readlines()
-    gold = [ int(each.strip()) for each in gold ]
+    gold = [int(each.strip()) for each in gold]
 
-with open(sys.argv[1], "r") as f:
+with open(sys.argv[2], "r") as f:
     pred = f.readlines()
-    pred = [ int(each.strip()) for each in pred ]
+    pred = [int(each.strip()) for each in pred]
 
 print(gold[:10])
 print(pred[:10])
