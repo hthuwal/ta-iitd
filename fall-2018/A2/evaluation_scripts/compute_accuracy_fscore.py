1e2e643fc6cc6ba8699c312a8f8aaaeaf019fc95
Harish Thuwal <hthuwal@gmail.com>
2018-09-22 14:37:40 +0530

    Print fscore

--- fall-2018/A2/evaluation_scripts/compute_accuracy_fscore.py
+++ fall-2018/A2/evaluation_scripts/compute_accuracy_fscore.py
@@ -9,8 +9,8 @@ with open(sys.argv[2], "r") as f:
     pred = f.readlines()
     pred = [int(float(each.strip())) for each in pred]
 
-with open(sys.argv[3], 'w') as fp:
-    acc = str(accuracy_score(gold, pred))
+if(len(gold) != len(pred)):
+    print('Number of labels are more than required' if pred.size > gold.size else 'Number of labels are less than required', end="")
+else:
     fscore = str(f1_score(gold, pred, average='macro'))
-    print("Accuracy: %s, Macro F-score: %s" % (acc, fscore))
-    fp.write(acc + "," + fscore)
+    print(fscore, end="")
