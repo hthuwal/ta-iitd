32e0288057ae99fa6df4ae6f33c9d01aadcf1016
Harish Thuwal <hthuwal@gmail.com>
2018-10-11 20:40:37 +0530

    Final Evaluation Script for q1

--- fall-2018/A2/evaluation_scripts/evaluate_q1.sh
+++ fall-2018/A2/evaluation_scripts/evaluate_q1.sh
@@ -1,84 +1,140 @@
 #!/bin/bash
 run()
 {
-    chmod +x $1
-    ./$@
+    timelimit="$1"
+    shift
+    logs="$1"
+    shift
+    chmod +x "$1"
+
+    echo -e "\nEvaluating Part $2"
+    echo -e "\nEvaluating Part $2" >> "$logs"
+    
+    time_start=$(date +%s)
+    timeout -k "$timelimit" "$timelimit" "./$@" &>> "$logs"
+    status=$?
+    time_end=$(date +%s)
+    
+    user_time=$(( time_end - time_start ))
+
+    if [ "$status" -eq 124 ]; then
+        echo "Status: Timed out"
+        echo "Status: Timed out" >> "$logs"
+    else
+        echo "Status: OK, Time taken: $user_time"
+        echo "Status: OK, Time taken: $user_time" >> "$logs"
+        
+    fi
+
+    write_score ",$timelimit,$user_time"
 }
 
 compute_score()
 {
-    : '
-        Compute score as per predicted values and write to given file
-        $1 python_file
-        $2 targets
-        $3 predicted
-        $4 outfile
-    '
-    python3 $1 $2 $3 $4 $5
+    
+    # Compute score as per predicted values and write to given file
+    # $1 python_file
+    # $2 targets
+    # $3 predicted
+    # $4 outfile
+    if [ -f "$3" ]; then
+        score=$(python3 "$1" "$2" "$3" "$4")
+    else
+        score="0.0"
+    fi
+    write_score ",$score"
 }
 
-main()
+write_score()
 {
-    : '
-        $1: data_dir
-        $2: sandbox_dir
-        $3: entry_number
-        $4: submissions_dir
-    '
-    main_dir=`pwd`
+    echo -n "$1" >> "$score_file"
+}
+
+evaluate()
+{
+    # $1: data_dir
+    # $2: sandbox_dir
+    # $3: entry_number
+    # $4: submissions_dir
+
+    main_dir=$(pwd)
+
+    if ! [ -d "$2" ]; then
+        mkdir -p "$2"
+    fi
+
     if [ -f "${4}/${3}.zip" ]; then
-        echo -e "Zip file found!"
-        unzip $4/$3.zip -d $2
-        zip_penalty="NO"
+        echo -e "\nExtracting ${3}.zip"
+        unzip -qq "$4/$3".zip -d "$2"
     elif [ -f "${4}/${3}.rar" ]; then
-        echo -e "RAR file found!"
-        unrar x "$4/$3.rar" $2
-        zip_penalty="YES"
+        echo -e "\nExtracting ${3}.rar"
+        unrar x "$4/$3.rar" "$2"
+    fi
+
+    entry_number="$3"
+
+    stud_folder_path=$(realpath "$2/$3")
+    logs=$(realpath "$2")"/logs"
+    score=$(realpath "$2")"/score"
+
+    if ! [ -d "$logs" ]; then
+        mkdir -p "$logs"
+    fi
+    if ! [ -d "$score" ]; then
+        mkdir -p "$score"
     fi
     
-    stud_folder_path=$(realpath "$2/$3")
+    logs="$logs/$entry_number"
+    score_file="$score/$entry_number"
     data_folder_path=$(realpath "$1")
     compute_accuracy=$(realpath "compute_accuracy.py")
     cd "$stud_folder_path"
 
+    
     status="OK"
     if [ -f "neural" ]; then
         fname="neural" 
         bash_fname_penalty="NO"
         dos2unix neural
     elif [ -f "neural.sh" ]; then
-        echo -e "Using neural.sh; Follow submission instructions."
         fname="neural.sh"
-        bash_fname_penalty="YES"
+        bash_fname_penalty="YES_sh"
         dos2unix neural.sh
     else
         status="NA"
-        echo -e "ERROR; neural or neural.sh not found!"
         bash_fname_penalty="YES"
     fi
 
-    echo -e "Zip Penalty: ${zip_penalty}\nBash_File_Name_Penalty: ${bash_fname_penalty}"
-    echo -e "${zip_penalty},${bash_fname_penalty}" > penalty
+    write_score "$entry_number"
+    write_score ",$bash_fname_penalty"
 
-
+    t1="2700"
+    t2="2700"
+    t3="2700"
+    
     if [ $status == "OK" ]; then
         part="a"
-        echo -e "\nEvaluating Part $part"
-        time run "$fname" "$part" "$data_folder_path/devnagri_train.csv" "$data_folder_path/devnagri_test.csv" "$stud_folder_path/predictions_nerual_$part" 512 0.01 "sigmoid" 100
-        compute_score "$compute_accuracy" "$data_folder_path/devnagri_target_labels.txt" "$stud_folder_path/predictions_nerual_$part" "$stud_folder_path/result_neural_$part" 
+        run "$t1" "$logs" "$fname" "$part" "$data_folder_path/devnagri_train.csv" "$data_folder_path/devnagri_test.csv" "$stud_folder_path/predictions_neural_${part}_1" 128 0.01 "sigmoid" 
+        compute_score "$compute_accuracy" "$data_folder_path/target_labels.txt" "$stud_folder_path/predictions_neural_${part}_1" "$stud_folder_path/result_neural_${part}_1" 
+
+        run "$t1" "$logs" "$fname" "$part" "$data_folder_path/devnagri_train.csv" "$data_folder_path/devnagri_test.csv" "$stud_folder_path/predictions_neural_${part}_2" 64 0.01 "relu" 100
+        compute_score "$compute_accuracy" "$data_folder_path/target_labels.txt" "$stud_folder_path/predictions_neural_${part}_2" "$stud_folder_path/result_neural_${part}_2" 
+        
+        run "$t1" "$logs" "$fname" "$part" "$data_folder_path/devnagri_train.csv" "$data_folder_path/devnagri_test.csv" "$stud_folder_path/predictions_neural_${part}_3" 64 0.1 "tanh" 10 10
+        compute_score "$compute_accuracy" "$data_folder_path/target_labels.txt" "$stud_folder_path/predictions_neural_${part}_3" "$stud_folder_path/result_neural_${part}_3" 
 
         part="b"
-        echo -e "\nEvaluating Part $part"
-        time run "$fname" "$part" "$data_folder_path/devnagri_train.csv" "$data_folder_path/devnagri_test.csv" "$stud_folder_path/predictions_nerual_$part"
-        compute_score "$compute_accuracy" "$data_folder_path/devnagri_target_labels.txt" "$stud_folder_path/predictions_nerual_$part" "$stud_folder_path/result_neural_$part" 
+        run "$t2" "$logs" "$fname" "$part" "$data_folder_path/devnagri_train.csv" "$data_folder_path/devnagri_test.csv" "$stud_folder_path/predictions_neural_$part"
+        compute_score "$compute_accuracy" "$data_folder_path/target_labels.txt" "$stud_folder_path/predictions_neural_$part" "$stud_folder_path/result_neural_$part" 
         
         part="c"
-        echo -e "\nEvaluating Part $part"
-        time run "$fname" "$part" "$data_folder_path/devnagri_train.csv" "$data_folder_path/devnagri_test.csv" "$stud_folder_path/predictions_nerual_$part"
-        compute_score "$compute_accuracy" "$data_folder_path/devnagri_target_labels.txt" "$stud_folder_path/predictions_nerual_$part" "$stud_folder_path/result_neural_$part" 
+        run "$t3" "$logs" "$fname" "$part" "$data_folder_path/devnagri_train.csv" "$data_folder_path/devnagri_test.csv" "$stud_folder_path/predictions_neural_$part"
+        compute_score "$compute_accuracy" "$data_folder_path/target_labels.txt" "$stud_folder_path/predictions_neural_$part" "$stud_folder_path/result_neural_$part" 
 
+    else
+        write_score ",$t1,NA,0.0,$t1,NA,0.0,$t1,NA,0.0,$t2,NA,0.0,$t3,NA,0.0"
     fi
-    cd $main_dir
+    cd "$main_dir"
 }
 
-main $1 $2 $3 $4
+evaluate "$@"
