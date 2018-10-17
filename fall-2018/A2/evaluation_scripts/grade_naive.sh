6db7023b4f713ff705c261036a48a21c3080b11d
Harish Thuwal <hthuwal@gmail.com>
2018-10-17 16:01:56 +0530

    Remove Faltu File

--- fall-2018/A2/evaluation_scripts/grade_naive.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-for i in {1..3}
-do
-	./auto_grade_q2.sh "../results/naive/set$i/logs" "../results/naive/set$i/scores" "../1801-COL341-A2 (Naive Bayes)-22000" "../submissions" "../data/naive/set$i" "../sandbox/naive/set$i"
-done
