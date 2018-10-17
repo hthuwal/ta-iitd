8a856da7e89384c52cbc2d73b3522f4537b6726b
Harish Thuwal <hthuwal@gmail.com>
2018-10-17 15:58:48 +0530

    Update The hyperparameters used

--- fall-2018/A2/evaluation_scripts/evaluate_q1.sh
+++ fall-2018/A2/evaluation_scripts/evaluate_q1.sh
@@ -104,23 +104,22 @@ evaluate()
         status="NA"
         bash_fname_penalty="YES"
     fi
-
     write_score "$entry_number"
     write_score ",$bash_fname_penalty"
 
-    t1="2700"
-    t2="2700"
-    t3="2700"
+    t1="5400"
+    t2="5400"
+    t3="5400"
     
     if [ $status == "OK" ]; then
         part="a"
-        run "$t1" "$logs" "$fname" "$part" "$data_folder_path/devnagri_train.csv" "$data_folder_path/devnagri_test.csv" "$stud_folder_path/predictions_neural_${part}_1" 128 0.01 "sigmoid" 
+        run "$t1" "$logs" "$fname" "$part" "$data_folder_path/devnagri_train.csv" "$data_folder_path/devnagri_test.csv" "$stud_folder_path/predictions_neural_${part}_1" 64 0.01 "sigmoid" 
         compute_score "$compute_accuracy" "$data_folder_path/target_labels.txt" "$stud_folder_path/predictions_neural_${part}_1" "$stud_folder_path/result_neural_${part}_1" 
 
-        run "$t1" "$logs" "$fname" "$part" "$data_folder_path/devnagri_train.csv" "$data_folder_path/devnagri_test.csv" "$stud_folder_path/predictions_neural_${part}_2" 64 0.01 "relu" 100
+        run "$t1" "$logs" "$fname" "$part" "$data_folder_path/devnagri_train.csv" "$data_folder_path/devnagri_test.csv" "$stud_folder_path/predictions_neural_${part}_2" 32 0.05 "relu" 128
         compute_score "$compute_accuracy" "$data_folder_path/target_labels.txt" "$stud_folder_path/predictions_neural_${part}_2" "$stud_folder_path/result_neural_${part}_2" 
         
-        run "$t1" "$logs" "$fname" "$part" "$data_folder_path/devnagri_train.csv" "$data_folder_path/devnagri_test.csv" "$stud_folder_path/predictions_neural_${part}_3" 64 0.1 "tanh" 10 10
+        run "$t1" "$logs" "$fname" "$part" "$data_folder_path/devnagri_train.csv" "$data_folder_path/devnagri_test.csv" "$stud_folder_path/predictions_neural_${part}_3" 32 0.5 "tanh" 256 64
         compute_score "$compute_accuracy" "$data_folder_path/target_labels.txt" "$stud_folder_path/predictions_neural_${part}_3" "$stud_folder_path/result_neural_${part}_3" 
 
         part="b"
